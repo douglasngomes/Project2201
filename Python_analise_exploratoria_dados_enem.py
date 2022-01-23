999ce0930d7d6469d5461a7ed367345ddc02bb65
Douglas Gomes <72231198+douglasngomes@users.noreply.github.com>
2022-01-23 15:52:06 -0300

    Update and rename analise_exploratoria_dados_enem.py to Python_analise_exploratoria_dados_enem.py

--- Python_analise_exploratoria_dados_enem.py
+++ Python_analise_exploratoria_dados_enem.py
@@ -2,14 +2,14 @@
 
 import pandas as pd
 
-fonte = r'C:\Users\Sabrina\Documents\Documents\Data_Science_ 2020\Python\MICRODADOS_ENEM_2019_SAMPLE_43278.csv'
+fonte = r'C:\MICRODADOS_ENEM_2019_SAMPLE_43278.csv'
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 
 dados = pd.read_csv()  # read é a variável para o pandas ler o arquivo csv
-dados.head()  # Head(cabeça), vai mostrar só o início das linhas, 5 por exemplo
+dados.head()  
 dados.shape()  # Toda extrutura do arquivo, QNT de linhas e colunas
 
 dados["SG_UF_RESIDENCIA"]  # Selecionando a coluna de dados que quero analizar, em uma lista
@@ -17,7 +17,7 @@ dados.columns.values  # Retorna todas as colunas do dataframe
 dados["SG_UF_RESIDENCIA"].unique()  # seleciona todos os estados mas não repete nenhum
 len(dados["SG_UF_RESIDENCIA"].value_counts())  # quantidade de estados, 27 por exemplo
 
-""" Vamos ver outras informações como a questão das idades dos candidatos """
+#Vamos ver outras informações como a questão das idades dos candidatos
 
 dados["NU_IDADE"].hist()  # visualização de dados do pandas em formato de gráficos
 dados["NU_IDADE"].hist(bins=30)  # Aumenta as informações com mais ranges dos dados
@@ -29,7 +29,7 @@ dados.query("IN_TREINEIRO == 1")["NU_IDADES"].value_counts()  # Quantidade de Tr
 dados["NU_NOTA_REDACAO"].mean()  # Média da nota da redação
 dados["NU_NOTA_REDACAO"].std()  # Desvio Padrão
 
-""" Vamos criar uma variável provas, para compararmos as notas """
+#Vamos criar uma variável provas, para compararmos as notas
 
 provas = ["NU_NOTA_CN", "NU_NOTA_CH", "NU_NOTA_MT", "NU_NOTA_REDACAO"]
 dados[provas].describe()  # Vai mostrar uma tabela das inf. estatístivas do pandas
@@ -38,7 +38,7 @@ dados["NU_NOTA_CN"].quantile(0.9)  # retorna 10% das notas mais altas, para as m
 dados["NU_NOTA_CN"].plot.box(grid=True, figsize=(8, 6))
 # Normalmente o boxplot é demonstrado em conjunto com outras notas, para facilitar a comparação visual.
 
-""" Quantos alunos com 14 anos ou menos temos por estado, e qual é a proporção? """
+#Quantos alunos com 14 anos ou menos temos por estado, e qual é a proporção?
 dados.query("NU_IDADE <= 14")["SG_UF_RESIDENCIA"].value_counts(normalize=True)
 
 # Para construirmos um gráfico com essa informação, seguimos abaixo:
@@ -56,7 +56,7 @@ dados[provas].sum(axis=1)  # Somando as notas de todas as provas do eixo '1', qu
 dados["NU_NOTAS_TOTAL"] = dados[provas].sum(axis=1)  # INCLUINDO UMA NOVA COLUNA NO NOSSO DATAFRAME(DADOS)
 dados.head()  # Visualizando o dataframe
 
-""" Criamos então o mesmo gráfico boxplot para vizualizarmos todas as notas"""
+#Criamos então o mesmo gráfico boxplot para vizualizarmos todas as notas
 plt.figure(figsize=(10, 8))
 renda_ordenada = dados['Q006'].unique()
 renda_ordenada.sort()
@@ -71,7 +71,7 @@ dados_sem_nota_zero.head()
 sns.boxplot(x='Q006', y='NU_NOTA_TOTAL', data=dados_sem_nota_zero, order=renda_ordenada)
 plt.title("Boxplot do total das notas pela renda")
 
-""" Vamos incluir mais uma variável na análise do boxplot, usando a função 'hue'"""
+#Vamos incluir mais uma variável na análise do boxplot, usando a função 'hue'
 plt.figure(figsize=(10, 8))
 renda_ordenada = dados['Q006'].unique()
 renda_ordenada.sort()
